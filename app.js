fbaba1847382f381c7eee97239136ec216935b80
maklei.filho <maklei.rf@gmail.com>
2023-04-01 11:18:12 -0400

    documentacao swagger com JSDoc implementada

--- app.js
+++ app.js
@@ -9,17 +9,17 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDefinition = {
   openapi: '3.0.0',
   info: {
-    title: 'Express API for JSONPlaceholder',
+    title: 'Documentação da api de encurtar urls',
     version: '1.0.0',
     description:
-      'This is a REST API application made with Express. It retrieves data from JSONPlaceholder.',
+      'Essa api encurta uma URL e retora um id de uma url encurtada para ser utilizada posteriormente',
     license: {
       name: 'Licensed Under MIT',
       url: 'https://spdx.org/licenses/MIT.html',
     },
     contact: {
-      name: 'JSONPlaceholder',
-      url: 'https://jsonplaceholder.typicode.com',
+      name: 'Maklei Alves',
+      url: 'https://github.com/MaakiZ',
     },
   },
   servers: [
--- routes/routes.js
+++ routes/routes.js
@@ -47,11 +47,100 @@ routes.get("/index", UrlController.listUrl);
 
 routes.get("/create", UrlController.createUrl);
 
+
+
+
+/**
+ * @swagger
+ * /openUrl:
+ *   get:
+ *     summary: Abre uma url encurtada
+ *     description: Abre uma url a partir de uma url encurtada.
+ *     requestBody:
+ *       description: Pega a url encurtada
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               urlId:
+ *                 type: string
+ *                 description: URL shortened.
+ *                 example: k9CQlUDEE
+ *     responses:
+ *       201:
+ *         description: Shortened URL created successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 origUrl:
+ *                   type: string
+ *                   description: The original URL.
+ *                   example: https://portalaluno.unyleya.edu.br
+ *       400:
+ *         description: Invalid request body or URL could not be shortened.
+ *       500:
+ *         description: Internal server error.
+ */
+
 routes.get('/openUrl', UrlController.openUrl);
 
 //routes.get('/:urlId', UrlController.findUrl);
 
 routes.post('/find', UrlController.findUrl);
+
+
+
+/**
+ * @swagger
+ * /short:
+ *   post:
+ *     summary: Encurta uma url.
+ *     description: Encurta uma url a partir de uma url completa.
+ *     requestBody:
+ *       description: Insere a url completa
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               origUrl:
+ *                 type: string
+ *                 description: Url completa para ser encurtada.
+ *                 example: https://portalaluno.unyleya.edu.br
+ *     responses:
+ *       201:
+ *         description: Url encurtada com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 origUrl:
+ *                   type: string
+ *                   description: A url original.
+ *                   example: https://portalaluno.unyleya.edu.br
+ *                 shortUrl:
+ *                   type: string
+ *                   description: Url encurtada
+ *                   example: http://localhost:3333/k9CQlUDEE
+ *                 urlId:
+ *                   type: string
+ *                   description: Id da URL.
+ *                   example: k9CQlUDEE
+ *                 date:
+ *                   type: string
+ *                   description: TData de criação.
+ *                   example: 01/04/2023 09:31:34
+ *       400:
+ *         description: Invalid request body or URL could not be shortened.
+ *       500:
+ *         description: Internal server error.
+ */
 
 routes.post("/short", ShortUrlController.shortUrl);
 
